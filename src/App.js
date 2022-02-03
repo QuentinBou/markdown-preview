da38d33555468603c766bcd13f62477421754edc
Quentin Bourgeois <quentintgc@live.fr>
2022-02-03 17:14:03 +0100

    end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { marked } from 'marked';
 
 function App() {
+
+  const [textValue, setTextValue] = useState('# `MarkDown Preview` \n  This is a React Project for transform **HTML** ! \n \n ## How to Use : \n \n \n * *italic* \n * **bold** \n * `background` \n * ~~crossed~~ \n \n * # h1 \n * ## h2 \n * ### h3 \n * #### etc')
+
+  function changeTextHandler(event){
+    setTextValue(event.target.value)
+  }
+
+  const markdownToHTML = () => {
+    return {__html: marked(textValue)}
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Welcome to my MarkDown Preview !</h1>
+      <div className='elements'>
+
+        <div className='element'>
+          <textarea value={textValue} onChange={e => changeTextHandler(e)}></textarea>
+        </div>
+
+        <div className='element'>
+          <div dangerouslySetInnerHTML={markdownToHTML()}></div>
+        </div>
+
+      </div>
     </div>
   );
 }
